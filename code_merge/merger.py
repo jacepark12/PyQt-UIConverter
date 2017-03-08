22e4b13f29f22ffa141952c5b96e0ae553704614
ParkJaesung <jspark141515@gmail.com>
2017-03-08 12:46:51 +0900

    Writing merger.py

--- code_merge/merger.py
+++ code_merge/merger.py
@@ -1,6 +1,9 @@
 import os
 # input type should be file directory
 
+# names of methods which will be changed
+MERGE_METHODS = ['def setupUi(self, MainWindow):']
+
 
 def updateUI(input, output):
 
@@ -14,22 +17,27 @@ def updateUI(input, output):
         with open(output, 'w+') as f:
             f.writelines(lines)
 
-
     else:
         merge(input, output)
-
 
     print('======convert complete')
 
 
-def merge(old, new):
+def merge(input, output):
     print('merging test')
 
-    input = []
-    with open(old, 'r') as f:
-        input = f.readlines()
+    input_lines = []
+    change_line_numbers = []
+    with open(input, 'r') as f:
+        input_lines = f.readlines()
 
-    print('input lines : ', input)
+    for line in input_lines:
+        if line in MERGE_METHODS:
+
+        else:
+
+
+    print('input lines : ', input_lines)
 
 
 if __name__ == "__main__":
